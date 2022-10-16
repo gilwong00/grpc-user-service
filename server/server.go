972af8b5277942cc58c4928a22d1529aeff52be8
Gilbert <gilwong3105@gmail.com>
2022-10-16 15:58:08 -0700

    Persist and read users from a json file

--- server/server.go
+++ server/server.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"context"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net"
+	"os"
 
 	pb "github.com/gilwong00/grpc-user-service/user"
 
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 const (
@@ -17,14 +20,75 @@ const (
 
 type UserService struct {
 	pb.UnimplementedUserServiceServer
+	// user_list *pb.UserList
+}
+
+func writeUserToFile(users *pb.UserList) error {
+	jsonBytes, err := protojson.Marshal(users)
+
+	if err != nil {
+		log.Fatalf("JSON Marshalling failed %v", err)
+		return err
+	}
+
+	if err := ioutil.WriteFile("users.json", jsonBytes, 0664); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s *UserService) CreateNewUser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
 	log.Printf("Received: %v", in.GetName())
+	readBytes, err := ioutil.ReadFile("users.json")
+	var users_list *pb.UserList = &pb.UserList{}
 	var user_id int32 = int32(rand.Intn(1000))
-	return &pb.User{
+	created_user := &pb.User{
 		Name: in.GetName(), Age: in.GetAge(), Id: user_id,
-	}, nil
+	}
+
+	// create users.json for the first time if file doesnt exist
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Print("File not found. Creating a new file")
+			users_list.Users = append(users_list.Users, created_user)
+
+			if err := writeUserToFile(users_list); err != nil {
+				log.Fatalf("Failed writing to file %v", err)
+			}
+
+			return created_user, nil
+		} else {
+			log.Fatalln("Error reading file: ", err)
+		}
+	}
+
+	if err := protojson.Unmarshal(readBytes, users_list); err != nil {
+		log.Fatalf("Failed to parse user list: %v", err)
+	}
+
+	users_list.Users = append(users_list.Users, created_user)
+
+	if err := writeUserToFile(users_list); err != nil {
+		log.Fatalf("Failed writing to file %v", err)
+	}
+
+	// s.user_list.Users = append(s.user_list.Users, created_user)
+	return created_user, nil
+}
+
+func (s *UserService) GetUsers(ctx context.Context, in *pb.GetUsersParams) (*pb.UserList, error) {
+	// return s.user_list, nil
+	jsonBytes, err := ioutil.ReadFile("users.json")
+	if err != nil {
+		log.Fatalf("Failed to read from file: %v", err)
+	}
+
+	var users_list *pb.UserList = &pb.UserList{}
+	if err := protojson.Unmarshal(jsonBytes, users_list); err != nil {
+		log.Fatalf("Unmarshalling failed: %v", err)
+	}
+	return users_list, nil
 }
 
 func main() {
@@ -34,7 +98,9 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	pb.RegisterUserServiceServer(s, &UserService{})
+	pb.RegisterUserServiceServer(s, &UserService{
+		// user_list: &pb.UserList{},
+	})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
--- user/user_grpc.pb.go
+++ user/user_grpc.pb.go
@@ -23,6 +23,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type UserServiceClient interface {
 	CreateNewUser(ctx context.Context, in *NewUser, opts ...grpc.CallOption) (*User, error)
+	GetUsers(ctx context.Context, in *GetUsersParams, opts ...grpc.CallOption) (*UserList, error)
 }
 
 type userServiceClient struct {
@@ -42,11 +43,21 @@ func (c *userServiceClient) CreateNewUser(ctx context.Context, in *NewUser, opts
 	return out, nil
 }
 
+func (c *userServiceClient) GetUsers(ctx context.Context, in *GetUsersParams, opts ...grpc.CallOption) (*UserList, error) {
+	out := new(UserList)
+	err := c.cc.Invoke(ctx, "/user.UserService/GetUsers", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // UserServiceServer is the server API for UserService service.
 // All implementations must embed UnimplementedUserServiceServer
 // for forward compatibility
 type UserServiceServer interface {
 	CreateNewUser(context.Context, *NewUser) (*User, error)
+	GetUsers(context.Context, *GetUsersParams) (*UserList, error)
 	mustEmbedUnimplementedUserServiceServer()
 }
 
@@ -56,6 +67,9 @@ type UnimplementedUserServiceServer struct {
 
 func (UnimplementedUserServiceServer) CreateNewUser(context.Context, *NewUser) (*User, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateNewUser not implemented")
+}
+func (UnimplementedUserServiceServer) GetUsers(context.Context, *GetUsersParams) (*UserList, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetUsers not implemented")
 }
 func (UnimplementedUserServiceServer) mustEmbedUnimplementedUserServiceServer() {}
 
@@ -88,6 +102,24 @@ func _UserService_CreateNewUser_Handler(srv interface{}, ctx context.Context, de
 	return interceptor(ctx, in, info, handler)
 }
 
+func _UserService_GetUsers_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetUsersParams)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(UserServiceServer).GetUsers(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/user.UserService/GetUsers",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(UserServiceServer).GetUsers(ctx, req.(*GetUsersParams))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // UserService_ServiceDesc is the grpc.ServiceDesc for UserService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -98,6 +130,10 @@ var UserService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "CreateNewUser",
 			Handler:    _UserService_CreateNewUser_Handler,
+		},
+		{
+			MethodName: "GetUsers",
+			Handler:    _UserService_GetUsers_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
